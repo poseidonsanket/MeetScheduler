498fed38f866cf35aedb2c5b21d996c1ae9d42c3
Sanket Dadali <sanketdadali05@gmail.com>
2024-07-05 11:06:20 +0530

    landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
-  return <div>Hi there</div>;
+  return (
+    <div>
+      <Header />
+      <Hero/>
+    </div>
+  );
 }
