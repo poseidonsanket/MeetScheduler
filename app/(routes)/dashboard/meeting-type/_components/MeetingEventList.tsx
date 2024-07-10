bda4238f376943d42031933efb7c6b9e29af31f5
Sanket Dadali <sanketdadali05@gmail.com>
2024-07-10 21:25:37 +0530

    bugs solved

--- app/(routes)/dashboard/meeting-type/_components/MeetingEventList.tsx
+++ app/(routes)/dashboard/meeting-type/_components/MeetingEventList.tsx
@@ -67,7 +67,7 @@ function MeetingEventList() {
     toast("Copied to Clicpboard");
   };
   return (
-    <div
+    <div  
       className="mt-10 grid grid-cols-1 md:grid-cols-2 
         lg:grid-cols-3 gap-7"
     >
@@ -147,7 +147,7 @@ function MeetingEventList() {
           </div>
         ))
       ) : (
-        <h2>Loading...</h2>
+        <h2>No meetings</h2>
       )}
     </div>
   );
